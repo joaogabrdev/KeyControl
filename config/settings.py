240a33e18f41f9fee6df239807e5990ed7cce496
Gabriel <joaogabrieloliveira.dev@gmail.com>
2026-09-24 15:47:12 -0300

    Segurança Https

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,11 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
+CSRF_TRUSTED_ORIGINS = [
+    os.getenv('ALLOWED_HOSTS', '').split(','),
+]
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
