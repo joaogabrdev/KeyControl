981fb0244c7dab594d7e00295f03f07a99b0be15
João Gabriel de Sousa <joaogabrieloliveira.dev@gmail.com>
2026-09-26 08:41:27 -0300

    SalasConfig

--- salas/apps.py
+++ salas/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class SalasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'salas'
+    verbose_name = 'Gestão de Salas'
