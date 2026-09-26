28dcb31834944dd9d1c1336a625a2587d52f8757
João Gabriel de Sousa <joaogabrieloliveira.dev@gmail.com>
2026-09-26 08:36:42 -0300

    Correção Salas 2

--- salas/models.py
+++ salas/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Salas(models.Model):
     numero = models.CharField(max_length=20, verbose_name="Número/Nome")
     status = models.BooleanField(default=False, verbose_name="Status da Sala")
+    
+    class Meta:
+        verbose_name = "Sala"
+        verbose_name_plural = "Salas"
 
     def __str__(self):
         return f"Sala {self.numero}"
